229d5a575e7f9fb8d2481837b07658345cffe38c
AdhikariManisha <urmanisha.ad@gmail.com>
2023-08-24 13:01:02 -0400

    Seeded Configuration

---
+++ aspnet-core/src/BishalAgroSeed.DbMigrator/Configurations/ConfigurationDataSeederContributor.cs
@@ -0,0 +1,35 @@
+﻿using BishalAgroSeed.Configurations;
+using BishalAgroSeed.Helpers;
+using System;
+using System.Threading.Tasks;
+using Volo.Abp.Data;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Domain.Repositories;
+
+namespace BishalAgroSeed.DbMigrator.Configurations;
+public class ConfigurationDataSeederContributor : IDataSeedContributor, ITransientDependency
+{
+    private readonly IRepository<Configuration> _repository;
+
+    public ConfigurationDataSeederContributor(IRepository<Configuration> repository)
+    {
+        _repository = repository;
+    }
+    public async Task SeedAsync(DataSeedContext context)
+    {
+        var data = DataHelper.GetValues<Constants.Configurations>();
+        foreach (var d in data)
+        {
+            var newConfiguration = new Configuration()
+            {
+                Key = d.value,
+                Value = d.description
+            };
+            var configuration = await _repository.FirstOrDefaultAsync(s => String.Equals(s.Key, newConfiguration.Key, StringComparison.OrdinalIgnoreCase));
+            if (configuration == null)
+            {
+                await _repository.InsertAsync(newConfiguration);
+            }
+        }
+    }
+}
---
+++ aspnet-core/src/BishalAgroSeed.Domain.Shared/Constants/Configurations.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace BishalAgroSeed.Constants;
+public class Configurations
+{
+    [Description("0.01")]
+    public const string DISCOUNT_PERCENT = "DISCOUNT_PERCENT";
+}
