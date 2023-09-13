e7f21b56e1dfec9b429e93276cb81a6147be2192
AdhikariManisha <urmanisha.ad@gmail.com>
2023-09-13 09:59:58 -0400

    Added nullable columns

--- aspnet-core/src/BishalAgroSeed.Application.Contracts/NumberGenerations/CreateUpdateNumberGenerationDto.cs
+++ aspnet-core/src/BishalAgroSeed.Application.Contracts/NumberGenerations/CreateUpdateNumberGenerationDto.cs
@@ -1,9 +1,13 @@
-﻿namespace BishalAgroSeed.NumberGenerations;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BishalAgroSeed.NumberGenerations;
 
 public class CreateUpdateNumberGenerationDto
 {
-    public string Prefix { get; set; }
+    public string? Prefix { get; set; }
+    [Required]
     public int Number { get; set; }
-    public string Suffix { get; set; }
+    public string? Suffix { get; set; }
+    [Required]
     public NumberGenerationTypes.NumberGenerationTypes NumberGenerationTypeId { get; set; }
 }
